ff8878e8e9cd14ca9cfdae27017a752ff63ccaf3
Luiz Miguel <luizmigueloficial1@gmail.com>
2022-07-19 16:03:50 -0300

    dates js

---
+++ JavaScript/dates/dates.js
@@ -0,0 +1,11 @@
+const dia = new Date();
+console.log(dia.getDate()); //dia do Mês
+
+const mes = new Date();
+console.log(mes.getMonth() +1); //Mês
+
+const ano = new Date();
+console.log(ano.getFullYear()); //ano
+
+const teste = new Date();
+console.log(teste.getTime());
